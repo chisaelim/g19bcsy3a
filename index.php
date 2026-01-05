eea42c401e3307978f78494116259e688a3f103b
chisaelim <chisae0123@gmail.com>
2026-01-05 10:39:21 +0700

    kae index lg

--- index.php
+++ index.php
@@ -3,6 +3,13 @@ include './includes/header.inc.php';
 include './includes/navbar.inc.php';
 ?>
 <h1>home</h1>
+<h1>home</h1>
+<h1>home</h1>
+<h1>home</h1>
+<h1>home</h1>
+<h1>home</h1>
+<h1>home</h1>
+<h1>home</h1>
 
 <?php
 include './includes/footer.inc.php';
